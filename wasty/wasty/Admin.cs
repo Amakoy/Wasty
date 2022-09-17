b8d8ad2e599e8187db459fc5607c03c72ff73b42
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-09-17 15:33:07 +0700

    add VS Project, Admin, and Waste class

---
+++ wasty/wasty/Admin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wasty
+{
+    class Admin
+    {
+        private string _accountID;
+        private string _userName;
+        private string _password;
+        private string _fullName;
+        private string _email;
+
+        public string AccountID {
+            get { return _accountID; }
+            set { _accountID = value; } 
+        }
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public string Name
+        {
+            get { return _fullName; }
+            set { _fullName = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        //methods
+        public Boolean Login(string userName, string password)
+        {
+            //dummy login database
+            if (userName == "Admin" & password == "Admin1010")
+            {
+                return true;
+            }
+            else if(userName == "User01" & password == "User01")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+        public void ShowCustomer()
+        {
+            
+        }
+        public void AddCustomer()
+        {
+            // nama, email, nohp
+        }
+        public void RemoveCustomer()
+        {
+
+        }
+        public void UpdateCustomer()
+        {
+
+        }
+
+        public void ShowRecords()
+        {
+
+        }
+    }
+}
---
+++ wasty/wasty/Waste.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wasty
+{
+    class Waste
+    {
+        private string _wasteID;
+        private string _wasteType;
+        private float _wastePrice;
+
+        public string WasteID
+        {
+            get { return _wasteID; }
+            set { _wasteID = value; }
+        }
+        public string WasteType
+        {
+            get { return _wasteType; }
+            set { _wasteType = value; }
+        }
+        public float WastePrice
+        {
+            get { return _wastePrice; }
+            set { _wastePrice = value; }
+        }
+
+        public void AddWaste(string wasteType, float wastePrice)
+        {
+
+        }
+        public void ShowWasteList()
+        {
+
+        }
+        public void DeleteWaste()
+        {
+
+        }
+        public void UpdateList()
+        {
+
+        }
+    }
+}
