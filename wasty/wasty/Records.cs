146f269fb8514a5e83f0e8180bc4e4802270adcc
Amakoy <71454818+Amakoy@users.noreply.github.com>
2022-09-17 16:47:30 +0700

    Update Records.cs

--- wasty/wasty/Records.cs
+++ wasty/wasty/Records.cs
@@ -1,38 +1,40 @@
-class records
+namespace wasty
 {
-    private string _recordId;
-    private string _accountID;
-    private DateTime _date;
-    private int _amountOfGoods;
-    private float _totalPrice;
+    class records
+    {
+        private string _recordId;
+        private string _accountID;
+        private DateTime _date;
+        private int _amountOfGoods;
+        private float _totalPrice;
 
-    public string RecordID
-    {
-        get { return _recordId; }
-        set { _recordId = value; }
-    }
+        public string RecordID
+        {
+            get { return _recordId; }
+            set { _recordId = value; }
+        }
 
-    public string AccountID
-    {
-        get { return _accountID; }
-        set { _accountID = value; }
-    }
-    public DateTime Date
-    {
-        get { return _date; }
-        set { _date = value; }
-    }
+        public string AccountID
+        {
+            get { return _accountID; }
+            set { _accountID = value; }
+        }
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
 
-    public int AmountOfGood
-    {
-        get { return _amountOfGoods; }
-        set { _amountOfGoods = value; }
-    }
+        public int AmountOfGood
+        {
+            get { return _amountOfGoods; }
+            set { _amountOfGoods = value; }
+        }
 
-    public float TotalPrice
-    {
-        get { return _totalPrice; }
-        set { _totalPrice = value; }
+        public float TotalPrice
+        {
+            get { return _totalPrice; }
+            set { _totalPrice = value; }
     }
 
 }
