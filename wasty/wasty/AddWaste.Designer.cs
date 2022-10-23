d0949b0827b84b22389237e73bfa42be401377ce
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-10-23 22:25:27 +0700

    Update "AddWaste" Form Design
    - add textboxes
    - add labels
    - add buttons
    - etc

---
+++ wasty/wasty/AddWaste.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace wasty
+{
+    partial class AddWaste
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbType = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.sidePanel.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(234)))), ((int)(((byte)(212)))));
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Controls.Add(this.lblClose);
+            this.mainPanel.Controls.Add(this.dataGridView1);
+            this.mainPanel.Controls.Add(this.btnAdd);
+            this.mainPanel.Controls.Add(this.tbPrice);
+            this.mainPanel.Controls.Add(this.label3);
+            this.mainPanel.Controls.Add(this.tbType);
+            this.mainPanel.Controls.Add(this.label2);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.mainPanel.Location = new System.Drawing.Point(253, -1);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1598, 954);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblClose.Location = new System.Drawing.Point(1480, 3);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(35, 45);
+            this.lblClose.TabIndex = 7;
+            this.lblClose.Text = "x";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(559, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(899, 544);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(192)))), ((int)(((byte)(123)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(186, 463);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Padding = new System.Windows.Forms.Padding(5);
+            this.btnAdd.Size = new System.Drawing.Size(207, 51);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add Sampah";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPrice.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbPrice.Location = new System.Drawing.Point(88, 387);
+            this.tbPrice.MinimumSize = new System.Drawing.Size(400, 40);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(400, 27);
+            this.tbPrice.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(88, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 35);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Harga Sampah";
+            // 
+            // tbType
+            // 
+            this.tbType.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbType.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbType.Location = new System.Drawing.Point(88, 286);
+            this.tbType.MinimumSize = new System.Drawing.Size(400, 40);
+            this.tbType.Name = "tbType";
+            this.tbType.Size = new System.Drawing.Size(400, 27);
+            this.tbType.TabIndex = 2;
+            this.tbType.TextChanged += new System.EventHandler(this.tbType_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(88, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jenis Sampah";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(549, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Jenis Sampah";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sidePanel
+            // 
+            this.sidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(192)))), ((int)(((byte)(123)))));
+            this.sidePanel.Controls.Add(this.panel4);
+            this.sidePanel.Controls.Add(this.panel3);
+            this.sidePanel.Controls.Add(this.panel2);
+            this.sidePanel.Controls.Add(this.panel1);
+            this.sidePanel.Location = new System.Drawing.Point(1, 0);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(253, 1037);
+            this.sidePanel.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(-6, 504);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(260, 90);
+            this.panel4.TabIndex = 8;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(60, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 45);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Records";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(-6, 415);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(260, 90);
+            this.panel3.TabIndex = 8;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(15, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(227, 45);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Add Customer";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(-6, 326);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 90);
+            this.panel2.TabIndex = 8;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(15, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 45);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Add Transaksi";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(-6, 237);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 90);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(77, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 45);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Home";
+            // 
+            // AddWaste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1782, 933);
+            this.Controls.Add(this.sidePanel);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddWaste";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddWaste";
+            this.Load += new System.EventHandler(this.AddWaste_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.sidePanel.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblClose;
+    }
+}
---
+++ wasty/wasty/AddWaste.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace wasty
+{
+    public partial class AddWaste : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            // untuk membuat border radius
+            int nLeftRect,          // x-coordinate of upper-left corner
+            int nTopRect,           // y-coordinate of upper-left corner
+            int nRightRect,         // x-coordinate of lower-right corner
+            int nBottonRect,        // y-coordinate of lower-right corner
+            int nWidthEllipse,       // width of ellipse
+            int nHeightEllipse       // height of ellipse
+        );
+
+
+        public AddWaste()
+        {
+            InitializeComponent();
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void AddWaste_Load(object sender, EventArgs e)
+        {
+            // btnAdd Style
+            btnAdd.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnAdd.Width, btnAdd.Height, 45, 45));
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.FlatAppearance.BorderSize = 0;
+
+            // text box Type Style
+            tbType.BorderStyle = BorderStyle.None;
+            tbType.AutoSize = false;
+            tbType.Size = new Size(400, 40);
+
+            // text box Price Style
+            tbPrice.BorderStyle = BorderStyle.None;
+            tbPrice.AutoSize = false;
+            tbPrice.Size = new Size(400, 40);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // panel border
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.White, ButtonBorderStyle.Solid);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            // panel border
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.White, ButtonBorderStyle.Solid);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            // panel border
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.White, ButtonBorderStyle.Solid);
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            // panel border
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.White, ButtonBorderStyle.Solid);
+        }
+
+        // Close Page
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Yakin ingin keluar?", "Exit Application", MessageBoxButtons.YesNo);
+            if(result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void tbType_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
