946708e4508cd9cc32d0fa721bbac12a9fa4eb18
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-10-24 08:46:16 +0700

    Update "LoginPage" Form Design
    Add components front end, belum config back end.
    
    Add close button.

--- wasty/wasty/LoginPage.Designer.cs
+++ wasty/wasty/LoginPage.Designer.cs
@@ -29,12 +29,151 @@ namespace wasty
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.lblBestie = new System.Windows.Forms.Label();
+            this.lblWasty = new System.Windows.Forms.Label();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.sidePanel.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sidePanel
+            // 
+            this.sidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(192)))), ((int)(((byte)(123)))));
+            this.sidePanel.Controls.Add(this.lblBestie);
+            this.sidePanel.Controls.Add(this.lblWasty);
+            this.sidePanel.Location = new System.Drawing.Point(-1, 0);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(516, 891);
+            this.sidePanel.TabIndex = 1;
+            // 
+            // lblBestie
+            // 
+            this.lblBestie.AutoSize = true;
+            this.lblBestie.Font = new System.Drawing.Font("Segoe UI", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBestie.Location = new System.Drawing.Point(90, 434);
+            this.lblBestie.Name = "lblBestie";
+            this.lblBestie.Size = new System.Drawing.Size(318, 133);
+            this.lblBestie.TabIndex = 2;
+            this.lblBestie.Text = "Bestie";
+            // 
+            // lblWasty
+            // 
+            this.lblWasty.AutoSize = true;
+            this.lblWasty.Font = new System.Drawing.Font("Segoe UI Semibold", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblWasty.Location = new System.Drawing.Point(87, 301);
+            this.lblWasty.Name = "lblWasty";
+            this.lblWasty.Size = new System.Drawing.Size(332, 133);
+            this.lblWasty.TabIndex = 1;
+            this.lblWasty.Text = "Wasty";
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(234)))), ((int)(((byte)(212)))));
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Controls.Add(this.lblClose);
+            this.mainPanel.Controls.Add(this.lblDesc);
+            this.mainPanel.Controls.Add(this.tbPassword);
+            this.mainPanel.Controls.Add(this.btnLogin);
+            this.mainPanel.Controls.Add(this.tbUsername);
+            this.mainPanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.mainPanel.Location = new System.Drawing.Point(514, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1251, 891);
+            this.mainPanel.TabIndex = 2;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblClose.Location = new System.Drawing.Point(1209, 5);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(26, 35);
+            this.lblClose.TabIndex = 12;
+            this.lblClose.Text = "x";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDesc.Location = new System.Drawing.Point(263, 226);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(710, 67);
+            this.lblDesc.TabIndex = 3;
+            this.lblDesc.Text = "Selamat Datang! Silakan Log In";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbPassword.Location = new System.Drawing.Point(399, 456);
+            this.tbPassword.MinimumSize = new System.Drawing.Size(450, 51);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PlaceholderText = "Password";
+            this.tbPassword.Size = new System.Drawing.Size(450, 44);
+            this.tbPassword.TabIndex = 11;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(192)))), ((int)(((byte)(123)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(518, 553);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(220, 50);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUsername.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbUsername.Location = new System.Drawing.Point(399, 368);
+            this.tbUsername.MinimumSize = new System.Drawing.Size(450, 51);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.PlaceholderText = "Username";
+            this.tbUsername.Size = new System.Drawing.Size(450, 44);
+            this.tbUsername.TabIndex = 7;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1764, 886);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.sidePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoginPage";
+            this.Load += new System.EventHandler(this.LoginPage_Load);
+            this.sidePanel.ResumeLayout(false);
+            this.sidePanel.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sidePanel;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label lblBestie;
+        private System.Windows.Forms.Label lblWasty;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblClose;
     }
 }
--- wasty/wasty/LoginPage.cs
+++ wasty/wasty/LoginPage.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices; // library untuk border radius
 
 namespace wasty
 {
@@ -13,6 +14,44 @@ namespace wasty
         public LoginPage()
         {
             InitializeComponent();
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20)); // border radius
+        }
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")] // import untuk border radius
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            // untuk membuat border radius
+            int nLeftRect,          // x-coordinate of upper-left corner
+            int nTopRect,           // y-coordinate of upper-left corner
+            int nRightRect,         // x-coordinate of lower-right corner
+            int nBottonRect,        // y-coordinate of lower-right corner
+            int nWidthEllipse,       // width of ellipse
+            int nHeightEllipse       // height of ellipse
+        );
+
+        private void LoginPage_Load(object sender, EventArgs e)
+        {
+            // btnAdd Style
+            btnLogin.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btnLogin.Width, btnLogin.Height, 45, 45));
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.FlatAppearance.BorderSize = 0;
+
+            // text box Type Style
+            tbUsername.BorderStyle = BorderStyle.None;
+            tbUsername.AutoSize = false;
+         
+            // text box Price Style
+            tbPassword.BorderStyle = BorderStyle.None;
+            tbPassword.AutoSize = false;
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Yakin ingin keluar?", "Exit Application", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
